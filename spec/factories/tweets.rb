5f27bd3c98c52911a04431b0839510d22949cf1d
mizu1228 <m.1228.jcm900@gmail.com>
2021-03-03 06:44:19 +0900

    tweetモデルのテストコード良し

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :tweet do
+    text           { 'test-tweet' }
+    association :user
     
+    after(:build) do |tweet|
+      tweet.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end 
   end
 end
