5880058ca72f65fca5078cb2e16b7a764dfe0b08
mizu1228 <m.1228.jcm900@gmail.com>
2021-03-16 13:31:59 +0900

    indexページでのいいね機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations:      'users/registrations',
 }
+  post 'like/:id' => 'likes#create', as: 'create_like'
+  delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
 
   resources :users, only: [:show]
   resources :tweets, only: [:new, :create, :show, :edit, :update, :destroy] do
