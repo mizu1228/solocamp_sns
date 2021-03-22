f11fd16a5de440e9a87ecea450d3da501b200bcb
mizu1228 <m.1228.jcm900@gmail.com>
2021-03-23 06:54:27 +0900

    非同期で、訪問した場所をチェックできるようにした

--- app/controllers/visited_sites_controller.rb
+++ app/controllers/visited_sites_controller.rb
@@ -1,12 +1,20 @@
 class VisitedSitesController < ApplicationController
+  before_action :camp_site_params
 
   def create
     VisitedSite.create(user_id: current_user.id, camp_site_id: params[:id])
-    redirect_to root_path
+    # binding.pry
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
     VisitedSite.find_by(user_id: current_user.id, camp_site_id: params[:id]).destroy
-    redirect_to root_path
+    # redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def camp_site_params
+    @site = CampSite.find(params[:id])
   end
 end
