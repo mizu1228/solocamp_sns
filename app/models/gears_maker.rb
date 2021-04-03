6c1f93f829c3e7a8fed83a6302f5d4bcb12738e1
mizu1228 <m.1228.jcm900@gmail.com>
2021-04-03 15:32:41 +0900

    placeholderを記入

--- app/models/gears_maker.rb
+++ app/models/gears_maker.rb
@@ -18,14 +18,6 @@ class GearsMaker
     super(attributes)
   end
 
-  # def save
-  #   gear = Gear.create(gear_genre_id: gear_genre_id, gear_name: gear_name, explanation: explanation, user_id: user_id, tweet_id: tweet_id, image: image)
-  #   maker = Maker.where(maker_name: maker_name).first_or_initialize
-  #   maker.save
-
-  #   GearMakerRelation.create(gear_id: gear.id, maker_id: maker.id)
-  # end
-
   def save(maker_list)
     ActiveRecord::Base.transaction do
       @gear.update(gear_genre_id: gear_genre_id, gear_name: gear_name, explanation: explanation, image: image, user_id: user_id, tweet_id: tweet_id)
