b7fdeaa23fcc673bd4bbc43ec75d86a3f2985248
mizu1228 <m.1228.jcm900@gmail.com>
2021-04-19 16:27:51 +0900

    Userのshowページを整えた

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ require("./preview")
 // require("../tag")
 require("../maker")
 require("../test")
+require("../user")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
