12f39af9c6511321e306b2e4c70103b8a68486e9
mizu1228 <m.1228.jcm900@gmail.com>
2021-07-11 08:47:12 +0900

    マップ機能の追加

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,3 +1,40 @@
 $(function () {
   $('#solo-camp-sns').fadeIn(9000);
 });
+
+
+// map's code
+
+$(function(){
+    //地域を選択
+    $('.area_btn').click(function(){
+        $('.area_overlay').show();
+        $('.pref_area').show();
+        var area = $(this).data('area');
+        $('[data-list]').hide();
+        $('[data-list="' + area + '"]').show();
+    });
+    
+    //レイヤーをタップ
+    $('.area_overlay').click(function(){
+        prefReset();
+    });
+    
+    //都道府県をクリック
+    $('.pref_list [data-id]').click(function(){
+        if($(this).data('id')){
+            var id = $(this).data('id');
+            document.getElementById('q_prefecture_id_eq').value = id
+            prefReset();
+        }
+    });
+    
+    //表示リセット
+    function prefReset(){
+        $('[data-list]').hide();
+        $('.pref_area').hide();
+        $('.area_overlay').hide();
+    }
+});
+
+// coded by JQuery
